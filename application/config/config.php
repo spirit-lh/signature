e70a20249d1594dd7d17f3b952127b808368feb6
lihe <lihe@192.168.1.167>
2016-03-05 11:08:22 +0800

    3333

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://192.168.1.105:8888/';
+$config['base_url'] = 'http://127.0.0.1:8888/';
 
 /*
 |--------------------------------------------------------------------------
